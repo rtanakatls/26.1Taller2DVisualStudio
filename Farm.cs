7b8913216c12eb79d58cafa62ac319847e57421c
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2026-04-15 17:38:38 -0500

    Added basic farm script

---
+++ Farm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2D261
+{
+    internal class Farm
+    {
+        private List<Plant> harvested;
+        private List<Plant> plots;
+        private int money;
+        private int plotSize;
+
+
+        public Farm()
+        {
+            plots = new List<Plant>();
+        }
+
+        public void PassTurn()
+        {
+            foreach (Plant plant in plots)
+            {
+                plant.PassTurn();
+            }
+        }
+
+        public void Harvest()
+        {
+            List<Plant> toHarvest = new List<Plant>();
+            foreach (Plant plant in plots)
+            {
+                if (plant.CanHarvest())
+                {
+                    toHarvest.Add(plant);
+                }
+            }
+
+            foreach (Plant plant in toHarvest)
+            { 
+                plots.Remove(plant);
+            }
+            foreach(Plant plant in toHarvest)
+            {
+                harvested.Add(plant);
+            }
+        }
+
+    }
+}
---
+++ Plant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2D261
+{
+    internal class Plant
+    {
+        private string name;
+        private int lifespan;
+        private int yield;
+        private int cost;
+        private int price;
+
+        private int age;
+
+        
+        public string Name { get { return name; } }
+        public int Yield { get { return yield; } }
+        public int Cost { get { return cost; } }
+        public int Price { get { return price; } }
+
+
+        public Plant(string name, int lifespan, int yield, int cost, int price)
+        {
+            this.name = name;
+            this.lifespan = lifespan;
+            this.yield = yield;
+            this.cost = cost;
+            this.price = price;
+        }
+
+        public void PassTurn()
+        {
+            age++;
+        }
+
+        public bool CanHarvest()
+        {
+            return age >= lifespan;
+        }
+
+
+
+    }
+}
